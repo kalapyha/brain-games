97bb4c55d01613d2b07ad0fc5db6b732643349f4
kalapyha <n.hapatnyukovsky@gmail.com>
2020-03-05 17:02:26 -0500

    refactoring

--- src/games/progression-game.js
+++ src/games/progression-game.js
@@ -4,11 +4,11 @@ import playGame from '..';
 const intro = 'What number is missing in the progression?';
 const progrCount = 9; // Amount of numbers inside the progression
 
-const createProgression = () => {
+const createProgression = (num) => {
   const progression = [];
   progression.push(randomNumber()); // Push first num to the array
   const iter = randomNumber(1, 20); // Creating another random num. It's gonna be our iterator.
-  for (let i = 0; i < progrCount; i += 1) { // less then 9 - progrLength
+  for (let i = 0; i < num; i += 1) { // less then progrCount - progrLength
     progression.push(progression[i] + iter); // next num in arr = prev num + iter
   }
   return progression;
@@ -23,7 +23,7 @@ const hideNumInProgressionArr = (arr) => {
 
 /* Game Round */
 const getRound = () => {
-  const progressionArr = createProgression();
+  const progressionArr = createProgression(progrCount);
   const [arr, num] = hideNumInProgressionArr(progressionArr);
   const question = arr.join(' ');
   const correctResult = num;
