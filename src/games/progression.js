d4a21c8418efd9e4f9da96ce692ec77e6a3ad126
kalapyha <n.hapatnyukovsky@gmail.com>
2020-03-11 16:17:32 -0400

    final commit

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,7 +4,8 @@ import playGame from '..';
 const intro = 'What number is missing in the progression?';
 const progressionCount = 10; // Amount of numbers inside the progression
 
-const createProgression = (first, step, iter) => ((iter === 0) ? [] : [first, ...createProgression(first + step, step, iter - 1)]);
+const createProgression = (first, step, iter) => ((iter === 0) ? [] :
+[first, ...createProgression(first + step, step, iter - 1)]);
 
 /* Game Round */
 const getRound = () => {
